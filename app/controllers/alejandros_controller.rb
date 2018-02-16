756721480a87514727fb45072e0f4c1177a539bb
Will Felix <willfelix93@gmail.com>
2018-02-16 03:29:33 -0300

    mercado pago assinatura (/pagos) + listagem alejandro refactor (/alejandros)

--- app/controllers/alejandros_controller.rb
+++ app/controllers/alejandros_controller.rb
@@ -1,101 +1,82 @@
 class AlejandrosController < ApplicationController
-  before_action :set_alejandro, only: [:show, :edit, :update, :destroy]
+	before_action :check_if_user_is_logged_in
+	before_action :set_alejandro, only: [:show, :edit, :update, :destroy]
 
-  # GET /alejandros
-  # GET /alejandros.json
-  def index
-    if user_signed_in? 
-      if current_user.email == "ale@ale.cl"
-        @alejandros = Alejandro.all
-      else
-        redirect_to shops_path
-      end
-    else
-      redirect_to shops_path
-    end
-  
-  end
+	# GET /alejandros
+	# GET /alejandros.json
+	def index
+		@alejandros = Alejandro.all
+	end
 
-  # GET /alejandros/1
-  # GET /alejandros/1.json
-  def show
-  end
+	# GET /alejandros/1
+	# GET /alejandros/1.json
+	def show
+	end
 
-  # GET /alejandros/new
-  def new
-    if user_signed_in? 
-      if current_user.email == "ale@ale.cl"
-        @alejandro = Alejandro.new
-        @members = Member.all
-      else
-        redirect_to shops_path
-      end
-    else
-      redirect_to shops_path
-    end
-  end
+	# GET /alejandros/new
+	def new
+		@alejandro = Alejandro.new
+		@members = Member.all
+	end
 
-  # GET /alejandros/1/edit
-  def edit
-    if user_signed_in? 
-      if current_user.email == "ale@ale.cl"
-        @members = Member.all
-      else
-        redirect_to shops_path
-      end
-    else
-      redirect_to shops_path
-    end
-  end
+	# GET /alejandros/1/edit
+	def edit
+		@members = Member.all
+	end
 
-  # POST /alejandros
-  # POST /alejandros.json
-  def create
-    @alejandro = Alejandro.new(alejandro_params)
+	# POST /alejandros
+	# POST /alejandros.json
+	def create
+		@alejandro = Alejandro.new(alejandro_params)
 
-    respond_to do |format|
-      if @alejandro.save
-        format.html { redirect_to @alejandro, notice: 'Alejandro was successfully created.' }
-        format.json { render :show, status: :created, location: @alejandro }
-      else
-        format.html { render :new }
-        format.json { render json: @alejandro.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+		respond_to do |format|
+			if @alejandro.save
+				format.html { redirect_to @alejandro, notice: 'Alejandro was successfully created.' }
+				format.json { render :show, status: :created, location: @alejandro }
+			else
+				format.html { render :new }
+				format.json { render json: @alejandro.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PATCH/PUT /alejandros/1
-  # PATCH/PUT /alejandros/1.json
-  def update
-    respond_to do |format|
-      if @alejandro.update(alejandro_params)
-        format.html { redirect_to @alejandro, notice: 'Alejandro was successfully updated.' }
-        format.json { render :show, status: :ok, location: @alejandro }
-      else
-        format.html { render :edit }
-        format.json { render json: @alejandro.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# PATCH/PUT /alejandros/1
+	# PATCH/PUT /alejandros/1.json
+	def update
+		respond_to do |format|
+			if @alejandro.update(alejandro_params)
+				format.html { redirect_to @alejandro, notice: 'Alejandro was successfully updated.' }
+				format.json { render :show, status: :ok, location: @alejandro }
+			else
+				format.html { render :edit }
+				format.json { render json: @alejandro.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # DELETE /alejandros/1
-  # DELETE /alejandros/1.json
-  def destroy
-    @alejandro.destroy
-    respond_to do |format|
-      format.html { redirect_to alejandros_url, notice: 'Alejandro was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+	# DELETE /alejandros/1
+	# DELETE /alejandros/1.json
+	def destroy
+		@alejandro.destroy
+		respond_to do |format|
+			format.html { redirect_to alejandros_url, notice: 'Alejandro was successfully destroyed.' }
+			format.json { head :no_content }
+		end
+	end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_alejandro
-      @alejandro = Alejandro.find(params[:id])
-    end
+	private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def alejandro_params
-      params.require(:alejandro).permit(:email, :payment, :payment_date, :due_date, :message, :member_id)
-    end
+	def check_if_user_is_logged_in
+		return redirect_to new_user_session_path unless user_signed_in?
+		return redirect_to root_path unless current_user.admin?
+	end
+
+	def set_alejandro
+		@alejandro = Alejandro.find(params[:id])
+	end
+
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def alejandro_params
+		params.require(:alejandro).permit(:email, :payment, :payment_date, :due_date, :message, :member_id)
+	end
 end

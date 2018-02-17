2487860ae6208ed15179ec525fa8a36d39cabe99
Will Felix <willfelix93@gmail.com>
2018-02-17 12:34:20 -0300

    sidekiq

--- app/models/order_status.rb
+++ app/models/order_status.rb
@@ -2,7 +2,7 @@ class OrderStatus < ActiveRecord::Base
 	belongs_to :order
 	belongs_to :user
 
-	# after_save :notify_users
+	after_save :notify_users
 
 	EN_CURSO = 'EN_CURSO'
 	ACEPTADO = 'ACEPTADO'
@@ -22,26 +22,8 @@ class OrderStatus < ActiveRecord::Base
 		order_status = self
 		order = order_status.order
 		client = order.user
-		seller = order.shop.user
-
-		if order_status.status == EN_CURSO
-			# Client create an order
-			OrderStatusMailer.buy(order_status, client).deliver_later
-
-		elsif order_status.status == FINALIZADO
-			# Seller finish an order
-			OrderStatusMailer.delivered(order_status, seller).deliver_later
-
-		end
-
-		# Check for who will receive
-		if order_status.user.id == seller.id
-			OrderStatusMailer.new_status_for_seller(order_status, client).deliver_later
-		else
-			OrderStatusMailer.new_status_for_client(order_status, seller).deliver_later
-		end
-
-		# StatusWorker.perform_async(self, client, seller)
+		
+		StatusWorker.perform_async(self, client, seller)
 
 		return true
 	end

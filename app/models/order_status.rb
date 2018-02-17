1775bda884fed017ea3ec689064c914de820af65
Will Felix <willfelix93@gmail.com>
2018-02-17 03:21:28 -0300

    comentando worker do order status

--- app/models/order_status.rb
+++ app/models/order_status.rb
@@ -23,7 +23,24 @@ class OrderStatus < ActiveRecord::Base
 		client = order.user
 		seller = order.shop.user
 
-		StatusWorker.perform_async(self, client, seller)
+		if order_status.status == EN_CURSO
+			# Client create an order
+			OrderStatusMailer.buy(order_status, client).deliver_later
+
+		elsif order_status.status == FINALIZADO
+			# Seller finish an order
+			OrderStatusMailer.delivered(order_status, seller).deliver_later
+
+		end
+
+		# Check for who will receive
+		if order_status.user.id == seller.id
+			OrderStatusMailer.new_status_for_seller(order_status, client).deliver_later
+		else
+			OrderStatusMailer.new_status_for_client(order_status, seller).deliver_later
+		end
+
+		# StatusWorker.perform_async(self, client, seller)
 
 		return true
 	end

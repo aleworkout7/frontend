067d9bb15902193094c93882992ed0116529b00e
alejandro <ale.gonzalezn@alumnos.duoc.cl>
2018-02-26 02:44:21 +0000

    modificado https a http

--- db/seeds.rb
+++
@@ -1,3 +0,0 @@
-if User.count === 0
-	User.create email: 'ale@ale.cl', password: 'admin123', password_confirmation: 'admin123', sign_in_count: '0', admin: true, confirmed_at: DateTime.now , confirmation_sent_at: DateTime.now
-end

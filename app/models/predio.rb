2ef9278f7851feeb37a5c8ee0ed9e303be9bde2d
alejandro <ale.gonzalezn@alumnos.duoc.cl>
2018-03-26 00:23:45 +0000

    predio home and model

--- app/models/predio.rb
+++ app/models/predio.rb
@@ -13,7 +13,7 @@ class Predio < ActiveRecord::Base
 			result = result	.joins("left join housing_sets hs on hs.id = predios.housing_set_id")
 							.joins("left join districts d on d.id = hs.district_id")
 							.joins("left join cities c on c.id = d.city_id")
-							.where("lower(predios.name) like :search or lower(hs.name) like :search or lower(d.name) like :search or lower(c.name) like :search", { search: "%#{search.downcase}%" })
+							.where("lower(predios.name) like :search or lower(predios.address) like :search or lower(details) like :search or lower(hs.name) like :search or lower(d.name) like :search or lower(c.name) like :search", { search: "%#{search.downcase}%" })
 							.distinct
 		end
 
